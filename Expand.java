bfbb7a83afb4f9e121850d9bb92fa0993f733d3f
cgm616 <cgm616@me.com>
2018-12-03 10:24:58 -0500

    Add logic and filehandling for expansion

--- Expand.java
+++ Expand.java
@@ -1,4 +1,6 @@
 import javafx.stage.Stage;
+import java.nio.file.Files;
+import java.io.IOException;
 
 public class Expand extends ColdenTab {
     public Expand(Stage stage) {
@@ -7,6 +9,23 @@ public class Expand extends ColdenTab {
 
     @Override
     public void run() {
-        System.out.println("Run called");
+        System.out.println("Running expansion on " + this.input + " and outputting into " + this.output);
+
+        byte[] inputData;
+
+        try {
+            inputData = Files.readAllBytes(this.input.toPath());
+        } catch (IOException e) {
+            System.out.println("There was an error");
+            inputData = new byte[0];
+        }
+
+        Artifact compressedData = Artifact.fromBytes(inputData);
+
+        Huffman expander = Huffman.deserialize(compressedData.getSerializedTree());
+
+        byte[] outputData = expander.expand(compressedData.getBody());
+
+        Artifact.writeBytes(output.toPath(), outputData);
     }
 }
