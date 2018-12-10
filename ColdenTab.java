5e7b50b793ee6a3e3ce10e29e6b13a9d0e94635d
cgm616 <cgm616@me.com>
2018-12-10 14:18:33 -0500

    Set text panes to be uneditable

--- ColdenTab.java
+++ ColdenTab.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -32,27 +31,27 @@ public class ColdenTab {
     Button runButton;
     Button clearInputButton;
     Button clearOutputButton;
-	Button clearLog;
+    Button clearLog;
     CheckBox saveGraph;
     TextArea log;
 
-	//Constructor requires a Stage object
+    // Constructor requires a Stage object
     public ColdenTab(Stage stage) {
         makeGUI(stage);
     }
-	
-	// 
-	/**
-	* This method is called from the constructor to 
-	* assign values and create the layout of the GUI.
-	* @param stage is the only parameter, it comes 
-	* from the Main class to provide a background for
-	* the rest of the javafx content.
-	* @return Nothing.
-	*/
+
+    //
+    /**
+     * This method is called from the constructor to assign values and create the
+     * layout of the GUI.
+     * 
+     * @param stage is the only parameter, it comes from the Main class to provide a
+     *              background for the rest of the javafx content.
+     * @return Nothing.
+     */
     public void makeGUI(Stage stage) {
-		
-		// Defining fields - all fields are used in creating the GUI
+
+        // Defining fields - all fields are used in creating the GUI
         this.stage = stage;
 
         this.grid = new GridPane();
@@ -64,22 +63,24 @@ public class ColdenTab {
         this.runButton = new Button("Run Colden");
         this.clearInputButton = new Button("Clear");
         this.clearOutputButton = new Button("Clear");
-		this.clearLog = new Button("Clear");
+        this.clearLog = new Button("Clear");
         this.log = new TextArea();
         this.saveGraph = new CheckBox("Save Huffman Tree Graph");
-	
-		// Set spacing of grid, so that objects are visually distinct and neat-looking
-        this.grid.setPadding(new Insets(10, 10, 10, 10)); // grid is contained by 10 pixels of empty space on all 4 sides
+
+        // Set spacing of grid, so that objects are visually distinct and neat-looking
+        this.grid.setPadding(new Insets(10, 10, 10, 10)); // grid is contained by 10 pixels of empty space on all 4
+                                                          // sides
         this.grid.setVgap(5); // Gap between objects above/below each other is 5 pixels
         this.grid.setHgap(5); // Gap between objects next to each other is 5 pixels
 
-        this.inputText.setDisable(true);
-        this.outputText.setDisable(true);
+        this.inputText.setEditable(false);
+        this.outputText.setEditable(false);
         this.log.setWrapText(true);
+        this.log.setEditable(false);
 
         // Adding objects to GridPane
-		// This code adds the objects to the grid
-		// in the correct order
+        // This code adds the objects to the grid
+        // in the correct order
         Label temp = new Label("Input");
         GridPane.setConstraints(temp, 0, 0);
         this.grid.getChildren().add(temp);
@@ -118,22 +119,23 @@ public class ColdenTab {
 
         GridPane.setConstraints(this.log, 0, 7);
         this.grid.getChildren().add(this.log);
-		
-		GridPane.setConstraints(this.clearLog, 1, 7);
-		this.grid.getChildren().add(this.clearLog);
-		// End of adding objects to grid.
-		
-		// When clicked, inputButton will create a pop-up window
-		// to let the user select an input file
+
+        GridPane.setConstraints(this.clearLog, 1, 7);
+        this.grid.getChildren().add(this.clearLog);
+        // End of adding objects to grid.
+
+        // When clicked, inputButton will create a pop-up window
+        // to let the user select an input file
         this.inputButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-			/**
-			* This method creates a FileChooser to allow the user
-			* to select an input file from File Explorer.
-			* @param e is the only parameter, lets the method know
-			* why it's being called.(the button has been clicked)
-			* @return nothing.
-			*/
+            /**
+             * This method creates a FileChooser to allow the user to select an input file
+             * from File Explorer.
+             * 
+             * @param e is the only parameter, lets the method know why it's being
+             *          called.(the button has been clicked)
+             * @return nothing.
+             */
             public void handle(final ActionEvent e) {
                 FileChooser chooser = createInputChooser();
                 File file = chooser.showOpenDialog(stage);
@@ -143,18 +145,19 @@ public class ColdenTab {
                 }
             }
         });
-		
-		// When clicked, outputButton will create a pop-up window
-		// to let the user select an output file 
+
+        // When clicked, outputButton will create a pop-up window
+        // to let the user select an output file
         this.outputButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-			/**
-			* This method creates a FileChooser to allow the user
-			* to select an output file from File Explorer.
-			* @param e is the only parameter, lets the method know
-			* why it's being called.(the button has been clicked)
-			* @return nothing.
-			*/
+            /**
+             * This method creates a FileChooser to allow the user to select an output file
+             * from File Explorer.
+             * 
+             * @param e is the only parameter, lets the method know why it's being
+             *          called.(the button has been clicked)
+             * @return nothing.
+             */
             public void handle(final ActionEvent e) {
                 FileChooser chooser = createOutputChooser();
                 File file = chooser.showSaveDialog(stage);
@@ -164,18 +167,19 @@ public class ColdenTab {
                 }
             }
         });
-		
-		// If input and output files have been selected, runButton
-		// will perform the Huffman compression/ expansion
+
+        // If input and output files have been selected, runButton
+        // will perform the Huffman compression/ expansion
         this.runButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-			/**
-			* This method calls run(), which compresses/expands the
-			* input file in the classes Compress and Expand.
-			* @param e is the only parameter, lets the method know
-			* why it's being called.(the button has been clicked)
-			* @return nothing.
-			*/
+            /**
+             * This method calls run(), which compresses/expands the input file in the
+             * classes Compress and Expand.
+             * 
+             * @param e is the only parameter, lets the method know why it's being
+             *          called.(the button has been clicked)
+             * @return nothing.
+             */
             public void handle(final ActionEvent e) {
                 if (input != null && output != null) {
                     run();
@@ -184,97 +188,95 @@ public class ColdenTab {
                 }
             }
         });
-		
-		// On click, clearInputButton clears the content of the inputText text field
+
+        // On click, clearInputButton clears the content of the inputText text field
         this.clearInputButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-			/**
-			* This method clears the content of the inputText
-			* text field.
-			* @param e is the only parameter, lets the method know
-			* why it's being called.(the button has been clicked)
-			* @return nothing.
-			*/
+            /**
+             * This method clears the content of the inputText text field.
+             * 
+             * @param e is the only parameter, lets the method know why it's being
+             *          called.(the button has been clicked)
+             * @return nothing.
+             */
             public void handle(final ActionEvent e) {
                 inputText.setText(""); // No selected input file exists now
             }
         });
 
-		// On click, clearOutputButton clears the content of the outputText text field
+        // On click, clearOutputButton clears the content of the outputText text field
         this.clearOutputButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-			/**
-			* This method clears the content of the outputText
-			* text field.
-			* @param e is the only parameter, lets the method know
-			* why it's being called.(the button has been clicked)
-			* @return nothing.
-			*/
+            /**
+             * This method clears the content of the outputText text field.
+             * 
+             * @param e is the only parameter, lets the method know why it's being
+             *          called.(the button has been clicked)
+             * @return nothing.
+             */
             public void handle(final ActionEvent e) {
                 outputText.setText(""); // No selected output file exists now
             }
         });
-		
-		// On click, clearLog wipes the text stored in the log
-		this.clearLog.setOnAction(new EventHandler<ActionEvent>() {
+
+        // On click, clearLog wipes the text stored in the log
+        this.clearLog.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(final ActionEvent e) {
                 log.setText(""); // Log is now emptied
             }
         });
     }
-	/**
-	* This method is just a stub; it will be
-	* overridden in subclasses Compress and
-	* Expand
-	* @param none.
-	* @return nothing.
-	*/
-    public void run() {}
 
-	/**
-	* Returns the GridPane field to display 
-	* on-screen in Main.
-	* @param none.
-	* @return Node - This returns the grid 
-	* field.
-	*/
+    /**
+     * This method is just a stub; it will be overridden in subclasses Compress and
+     * Expand
+     * 
+     * @param none.
+     * @return nothing.
+     */
+    public void run() {
+    }
+
+    /**
+     * Returns the GridPane field to display on-screen in Main.
+     * 
+     * @param none.
+     * @return Node - This returns the grid field.
+     */
     public Node render() {
         return this.grid;
     }
-	
-	/**
-	* Prints any input messages to the console 
-	* and adds them to the log.
-	* @param message is the first parameter, and
-	* is the content of the message to print.
-	* @param prio is the second, specifies the
-	* type of message being printed.
-	* @return nothing.
-	*/
+
+    /**
+     * Prints any input messages to the console and adds them to the log.
+     * 
+     * @param message is the first parameter, and is the content of the message to
+     *                print.
+     * @param prio    is the second, specifies the type of message being printed.
+     * @return nothing.
+     */
     public void log(String message, Level prio) {
         System.out.println("[ " + prio.getName() + " ]: " + message);
         this.log.appendText("[ " + prio.getName() + " ]: " + message + "\n");
     }
-	
-	/**
-	* Gets the fileChooser field to select an
-	* input file.
-	* @param none.
-	* @return FileChooser - returns this object's
-	* fileChooser field
-	*/
+
+    /**
+     * Gets the fileChooser field to select an input file.
+     * 
+     * @param none.
+     * @return FileChooser - returns this object's fileChooser field
+     */
     private FileChooser createInputChooser() {
         return this.fileChooser;
     }
 
-	/**
-	* Gets the fileChooser field to select an
-	* output file.
-	* @param none.
-	* @return FileChooser - returns this object's
-	* fileChooser field
-	*/
+    /**
+     * Gets the fileChooser field to select an output file.
+     * 
+     * @param none.
+     * @return FileChooser - returns this object's fileChooser field
+     */
     private FileChooser createOutputChooser() {
         return this.fileChooser;
     }
