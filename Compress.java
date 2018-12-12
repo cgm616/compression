7c57a54be8f4ea8116cf5f5a933d04637df54b4a
cgm616 <cgm616@me.com>
2018-12-12 14:41:37 -0500

    Move heavy processing logic to background threads

--- Compress.java
+++ Compress.java
@@ -3,8 +3,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.logging.Level;
 
+import javafx.concurrent.Task;
 import javafx.stage.Stage;
 
+/**
+ * The GUI handler for the Compress tab, containing the logic required to power
+ * that tab.
+ */
 public class Compress extends ColdenTab {
 
     public Compress(Stage stage) {
@@ -13,75 +18,87 @@ public class Compress extends ColdenTab {
 
     @Override
     public void run() {
-        log("Running compression... (input: " + this.input.getPath() + ", output: " + this.output.getPath() + ")",
-                Level.INFO);
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                log("Running compression... (input: " + input.getPath() + ", output: " + output.getPath() + ")",
+                        Level.INFO);
 
-        byte[] inputData;
+                byte[] inputData;
 
-        try {
-            inputData = Files.readAllBytes(this.input.toPath());
-        } catch (IOException e) {
-            log("Input file could not be read: " + e.getMessage() + ". Aborting.", Level.SEVERE);
-            return;
-        }
-
-        if (inputData.length < 1) {
-            log("Please select an input file that is longer than 0 bytes. Aborting.", Level.SEVERE);
-            return;
-        } else {
-            log("Compressing " + inputData.length + " bytes...", Level.INFO);
-        }
-
-        Huffman compressor;
-
-        try {
-            compressor = new Huffman(inputData);
-        } catch (Exception e) {
-            log("Huffman tree could not be made: " + e.getMessage() + ". Aborting.", Level.SEVERE);
-            return;
-        }
-
-        log("Huffman tree successfully constructed...", Level.INFO);
-
-        if (this.saveGraph.isSelected()) { // TODO: some checkbox for writing to a graph, as well as getting the file
-            File graphOutput = new File(this.tree.getPath().substring(0, this.tree.getPath().length() - 3) + "pdf");
-
-            if (graphOutput.exists()) {
-                log("Graph output file already exists. Graph will not be written.", Level.WARNING);
-            } else {
                 try {
-                    Graph graph = new Graph(compressor);
-                    graph.write(graphOutput);
+                    inputData = Files.readAllBytes(input.toPath());
                 } catch (IOException e) {
-                    log("Could not write Huffman tree to graph (file: " + graphOutput.getPath() + "): " + e.getMessage()
-                            + ". Expansion will continue.", Level.WARNING);
+                    log("Input file could not be read: " + e.getMessage() + ". Aborting.", Level.SEVERE);
+                    return null;
                 }
+
+                if (inputData.length < 1) {
+                    log("Please select an input file that is longer than 0 bytes. Aborting.", Level.SEVERE);
+                    return null;
+                } else {
+                    log("Compressing " + inputData.length + " bytes...", Level.INFO);
+                }
+
+                Huffman compressor;
+
+                try {
+                    compressor = new Huffman(inputData);
+                } catch (Exception e) {
+                    log("Huffman tree could not be made: " + e.getMessage() + ". Aborting.", Level.SEVERE);
+                    return null;
+                }
+
+                log("Huffman tree successfully constructed...", Level.INFO);
+
+                if (saveGraph.isSelected()) { // TODO: some checkbox for writing to a graph, as well as getting the
+                                              // file
+                    File graphOutput = new File(tree.getPath().substring(0, tree.getPath().length() - 3) + "pdf");
+
+                    if (graphOutput.exists()) {
+                        log("Graph output file already exists. Graph will not be written.", Level.WARNING);
+                    } else {
+                        try {
+                            Graph graph = new Graph(compressor);
+                            graph.write(graphOutput);
+                        } catch (IOException e) {
+                            log("Could not write Huffman tree to graph (file: " + graphOutput.getPath() + "): "
+                                    + e.getMessage() + ". Expansion will continue.", Level.WARNING);
+                        }
+                    }
+
+                    log("Graph output file written...", Level.INFO);
+                }
+
+                byte[] outputBody;
+
+                try {
+                    outputBody = compressor.compress(inputData);
+                } catch (Exception e) {
+                    log("Could not compress data with the Huffman tree: " + e.getMessage() + ". Aborting.",
+                            Level.SEVERE);
+                    return null;
+                }
+
+                Artifact outputData = Artifact.build(compressor, outputBody);
+
+                double compressionRatio = ((double) outputData.bytes.length * 8) / (double) inputData.length;
+
+                log("Data successfully compressed with a ratio of " + compressionRatio + " bits per byte...",
+                        Level.INFO);
+
+                try {
+                    outputData.writeToPath(output.toPath());
+                } catch (IOException e) {
+                    log("Could not write compressed file: " + e.getMessage() + ". Aborting.", Level.SEVERE);
+                }
+
+                log("Compression and output done.", Level.INFO);
+
+                return null;
             }
+        };
 
-            log("Graph output file written...", Level.INFO);
-        }
-
-        byte[] outputBody;
-
-        try {
-            outputBody = compressor.compress(inputData);
-        } catch (Exception e) {
-            log("Could not compress data with the Huffman tree: " + e.getMessage() + ". Aborting.", Level.SEVERE);
-            return;
-        }
-
-        Artifact outputData = Artifact.build(compressor, outputBody);
-
-        double compressionRatio = ((double) outputData.bytes.length * 8) / (double) inputData.length;
-
-        log("Data successfully compressed with a ratio of " + compressionRatio + " bits per byte...", Level.INFO);
-
-        try {
-            outputData.writeToPath(this.output.toPath());
-        } catch (IOException e) {
-            log("Could not write compressed file: " + e.getMessage() + ". Aborting.", Level.SEVERE);
-        }
-
-        log("Compression and output done.", Level.INFO);
+        new Thread(task).start();
     }
 }
--- Expand.java
+++ Expand.java
@@ -3,8 +3,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.logging.Level;
 
+import javafx.concurrent.Task;
 import javafx.stage.Stage;
 
+/**
+ * The GUI handler for the Expand tab, containing the logic required to power
+ * that tab.
+ */
 public class Expand extends ColdenTab {
 
     public Expand(Stage stage) {
@@ -13,81 +18,91 @@ public class Expand extends ColdenTab {
 
     @Override
     public void run() {
-        log("Running expansion... (input: " + this.input.getPath() + ", output: " + this.output.getPath() + ")",
-                Level.INFO);
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                log("Running expansion... (input: " + input.getPath() + ", output: " + output.getPath() + ")",
+                        Level.INFO);
 
-        byte[] inputData;
+                byte[] inputData;
 
-        try {
-            inputData = Files.readAllBytes(this.input.toPath());
-        } catch (IOException e) {
-            log("Input file could not be read: " + e.getMessage() + ". Aborting.", Level.SEVERE);
-            return;
-        }
-
-        if (inputData.length <= 10) {
-            log("Please select an input file that is longer than 10 bytes (the minimum compressed filesize). Aborting.",
-                    Level.SEVERE);
-            return;
-        } else {
-            log("Expanding " + inputData.length + " bytes...", Level.INFO);
-        }
-
-        Artifact compressedData;
-
-        try {
-            compressedData = Artifact.fromBytes(inputData);
-        } catch (Exception e) {
-            log("The input file selected is malformed: " + e.getMessage() + ". Aborting.", Level.SEVERE);
-            return;
-        }
-
-        byte[] tree = compressedData.getSerializedTree();
-        byte[] body = compressedData.getBody();
-
-        log("Input file successfully parsed (tree length: " + tree.length + ", body length: " + body.length + ")...",
-                Level.INFO);
-
-        Huffman expander;
-
-        try {
-            expander = Huffman.deserialize(tree);
-        } catch (Exception e) {
-            log("Huffman tree could not be deserialized: " + e.getMessage() + ". Aborting.", Level.SEVERE);
-            return;
-        }
-
-        log("Huffman tree successfully deserialized from file...", Level.INFO);
-
-        if (this.saveGraph.isSelected()) { // TODO: some checkbox for writing to a graph, as well as getting the file
-            File graphOutput = new File(this.tree.getPath().substring(0, this.tree.getPath().length() - 3) + "pdf");
-
-            if (graphOutput.exists()) {
-                log("Graph output file already exists. Graph will not be written.", Level.WARNING);
-            } else {
                 try {
-                    Graph graph = new Graph(expander);
-                    graph.write(graphOutput);
+                    inputData = Files.readAllBytes(input.toPath());
                 } catch (IOException e) {
-                    log("Could not write Huffman tree to graph (file: " + graphOutput.getPath() + "): " + e.getMessage()
-                            + ". Expansion will continue.", Level.WARNING);
+                    log("Input file could not be read: " + e.getMessage() + ". Aborting.", Level.SEVERE);
+                    return null;
                 }
+
+                if (inputData.length <= 10) {
+                    log("Please select an input file that is longer than 10 bytes (the minimum compressed filesize). Aborting.",
+                            Level.SEVERE);
+                    return null;
+                } else {
+                    log("Expanding " + inputData.length + " bytes...", Level.INFO);
+                }
+
+                Artifact compressedData;
+
+                try {
+                    compressedData = Artifact.fromBytes(inputData);
+                } catch (Exception e) {
+                    log("The input file selected is malformed: " + e.getMessage() + ". Aborting.", Level.SEVERE);
+                    return null;
+                }
+
+                byte[] treeData = compressedData.getSerializedTree();
+                byte[] body = compressedData.getBody();
+
+                log("Input file successfully parsed (tree length: " + treeData.length + ", body length: " + body.length
+                        + ")...", Level.INFO);
+
+                Huffman expander;
+
+                try {
+                    expander = Huffman.deserialize(treeData);
+                } catch (Exception e) {
+                    log("Huffman tree could not be deserialized: " + e.getMessage() + ". Aborting.", Level.SEVERE);
+                    return null;
+                }
+
+                log("Huffman tree successfully deserialized from file...", Level.INFO);
+
+                if (saveGraph.isSelected()) { // TODO: some checkbox for writing to a graph, as well as getting the
+                                              // file
+                    File graphOutput = new File(tree.getPath().substring(0, tree.getPath().length() - 3) + "pdf");
+
+                    if (graphOutput.exists()) {
+                        log("Graph output file already exists. Graph will not be written.", Level.WARNING);
+                    } else {
+                        try {
+                            Graph graph = new Graph(expander);
+                            graph.write(graphOutput);
+                        } catch (IOException e) {
+                            log("Could not write Huffman tree to graph (file: " + graphOutput.getPath() + "): "
+                                    + e.getMessage() + ". Expansion will continue.", Level.WARNING);
+                        }
+                    }
+
+                    log("Graph output file written...", Level.INFO);
+                }
+
+                byte[] outputData = expander.expand(body);
+
+                log("Compressed input successfully expanded into " + outputData.length + " bytes...", Level.INFO);
+
+                try {
+                    Artifact.writeBytes(output.toPath(), outputData);
+                } catch (Exception e) {
+                    log("Could not write expanded file: " + e.getMessage() + ". Aborting.", Level.SEVERE);
+                    return null;
+                }
+
+                log("Expansion and output done.", Level.INFO);
+
+                return null;
             }
+        };
 
-            log("Graph output file written...", Level.INFO);
-        }
-
-        byte[] outputData = expander.expand(body);
-
-        log("Compressed input successfully expanded into " + outputData.length + " bytes...", Level.INFO);
-
-        try {
-            Artifact.writeBytes(output.toPath(), outputData);
-        } catch (Exception e) {
-            log("Could not write expanded file: " + e.getMessage() + ". Aborting.", Level.SEVERE);
-            return;
-        }
-
-        log("Expansion and output done.", Level.INFO);
+        new Thread(task).start();
     }
 }
