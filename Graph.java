1feed111dc88aabe3eeb202d66ce03d8a0f9b099
cgm616 <cgm616@me.com>
2018-12-11 21:51:51 -0500

    Comment Graph.java

--- Graph.java
+++ Graph.java
@@ -7,23 +7,41 @@ import java.nio.charset.StandardCharsets;
 public class Graph {
     Huffman tree;
 
+    /**
+     * Class constructor for the Graph
+     * 
+     * @param tree The Huffman tree to which this Graph refers
+     * @return An instance of Graph that can print this tree
+     */
     public Graph(Huffman tree) {
         this.tree = tree;
     }
 
+    /**
+     * This method writes a visualization of the tree to a file. It relies on the
+     * presence of the `dot` command in the path to convert from a graphviz textual
+     * representation to a pdf file
+     * 
+     * @param file The byte array input containing the data to be expanded
+     * @exception IOException Thrown when the file already exists, or when IO errors
+     */
     public void write(File file) throws IOException {
-        // Make new process of `dot` and then feed it the written graph to directly
-        // produce pdfs.
-
+        // Check that the file doesn't already exist
         if (file.exists()) {
+            // If it does, throw an exception
             throw new IOException("File " + file.getPath() + " already exists");
         }
 
+        // Create and run a new process of `dot` that accepts input from this program
+        // and outputs data into the file passed in
         ProcessBuilder pb = new ProcessBuilder("dot", "-Tpdf");
         pb.directory(file.getParentFile());
         pb.redirectOutput(file);
         Process p = pb.start();
 
+        // Call the tree's internal method that writes a textual representation of
+        // itself to a given writer, in this case passing in the stdin to the process of
+        // `dot`
         this.tree.writeToGraph(new BufferedWriter(new OutputStreamWriter(p.getOutputStream(), StandardCharsets.UTF_8)));
     }
 }
